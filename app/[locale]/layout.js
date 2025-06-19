7507511167197cbbec9e3a0bd6f806b7250ddcee
dreamerview <temirkhan.onyx@gmail.com>
2025-06-19 10:39:28 +0500

    removed js from layout

--- app/[locale]/layout.js
+++ app/[locale]/layout.js
@@ -16,10 +16,6 @@ export default async function RootLayout({ children, params }) {
         <PreloaderComponent locale={locale} />
         <HeaderSection locale={locale} />
         {children}
-        <Script
-          src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-          strategy="afterInteractive"
-        />
       </body>
     </html>
   );
